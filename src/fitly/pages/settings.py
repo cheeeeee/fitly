fad7ee2d3f0c63c97b35d0d6540c037230d9f21b
Will Sparks <will.sparks@swsparks.com>
2026-03-29 22:36:44 -0500

    Fix UnboundLocalError by indenting save_strava_token

--- src/fitly/pages/settings.py
+++ src/fitly/pages/settings.py
@@ -1228,7 +1228,7 @@ def update_tokens(n_clicks, search):
                                                                         client_secret=config.get('strava',
                                                                                                  'client_secret'),
                                                                         code=parse_qs(query_params.query)['code'][0])
-        save_strava_token(token_response)
+            save_strava_token(token_response)
 
     if 'withings' in search:
         query_params = urlparse.urlparse(search.replace('withings&', ''))
